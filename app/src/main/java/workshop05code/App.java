91ec1be66bc40d14d36a25de7e3cc9f8d31bcab3
Jaykallam <100818738+Jaykallam@users.noreply.github.com>
2025-03-27 14:17:40 +1100

    changed path

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -21,7 +21,7 @@ public class App {
         // must set before the Logger
         // loads logging.properties from the classpath
         try {// resources\logging.properties
-            LogManager.getLogManager().readConfiguration(new FileInputStream("resources/logging.properties"));
+            LogManager.getLogManager().readConfiguration(new FileInputStream("/Users/jay/Documents/w05sqlinjectionpub-Jaykallam/resources/logging.properties"));
         } catch (SecurityException | IOException e1) {
             e1.printStackTrace();
         }
@@ -36,7 +36,7 @@ public class App {
     public static void main(String[] args) {
         SQLiteConnectionManager wordleDatabaseConnection = new SQLiteConnectionManager("words.db");
 
-        wordleDatabaseConnection.createNewDatabase("words.db");
+        wordleDatabaseConnection.createNewDatabase("/Users/jay/Documents/w05sqlinjectionpub-Jaykallam/sqlite/words.db");
         if (wordleDatabaseConnection.checkIfConnectionDefined()) {
             System.out.println("Wordle created and connected.");
         } else {
@@ -52,7 +52,7 @@ public class App {
 
         // let's add some words to valid 4 letter words from the data.txt file
 
-        try (BufferedReader br = new BufferedReader(new FileReader("resources/data.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/jay/Documents/w05sqlinjectionpub-Jaykallam/resources/data.txt"))) {
             String line;
             int i = 1;
             while ((line = br.readLine()) != null) {
